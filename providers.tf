8c925a50d55b1124250a9c5c1a5844713ed06802
RjDev <rjdevnic@gmail.com>
2023-12-24 21:17:10 +0530

    adding kubectl server access to eks cluster

--- providers.tf
+++ providers.tf
@@ -16,6 +16,11 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.5.1"
     }
+    
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
