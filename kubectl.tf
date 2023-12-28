4954727dd8230530ffba87ba5961f0c573301b43
RjDev <rjdevnic@gmail.com>
2023-12-28 07:50:10 +0530

    adding kubectl server access to eks cluster

--- kubectl.tf
+++ kubectl.tf
@@ -21,23 +21,34 @@ YAML
 }
 
 */
-/*
+
 
 resource "kubectl_manifest" "aws_auth" {
   yaml_body = <<YAML
 apiVersion: v1
 kind: ConfigMap
+data:
+  mapRoles: |
+    - groups:
+      - system:bootstrappers
+      - system:nodes
+      rolearn: ${aws_iam_role.eks_node_group.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+    - groups:
+      - system:masters
+      rolearn: ${aws_iam_role.kubectl_ssm_role.arn}
+      username: system:node:{{EC2PrivateDNSName}}
 metadata:
   labels:
     app.kubernetes.io/managed-by: Terraform
   name: aws-auth
   namespace: kube-system
-${local.aws_auth_configmap_yaml}
+
 YAML
 
-depends_on = [data.http.wait_for_cluster]
+depends_on = [aws_eks_cluster.cluster]
 }
-
+/*
 locals {
   aws_user_role = "arn:aws:iam::1234:role/AWSReservedSSO_AWSAdministratorAccess_ce861bcf52b0eabc"
   aws_terraform_role = "arn:aws:iam::1234:role/terraform-role-dev"
