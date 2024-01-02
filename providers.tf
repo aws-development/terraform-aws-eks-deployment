64a1394dd2ede41b4dcd0f4a415b970d4c9f0e78
RjDev <rjdevnic@gmail.com>
2024-01-02 16:50:03 +0530

    adding the wiz k8 admission controller

--- providers.tf
+++ providers.tf
@@ -45,7 +45,7 @@ provider "kubectl" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.default.token
-  load_config_file       = false
+  #load_config_file       = false
 }
 
 
