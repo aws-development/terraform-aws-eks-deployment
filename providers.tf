329fcf3c35d2b0e0dd1ccb4bd522dd56bf1ca707
RjDev <rjdevnic@gmail.com>
2024-01-02 13:34:57 +0530

    adding the wiz k8 admission controller

--- providers.tf
+++ providers.tf
@@ -45,7 +45,7 @@ provider "kubectl" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.default.token
-  load_config_file       = false
+  load_config_file       = true
 }
 
 
@@ -68,12 +68,12 @@ provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.default.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.default.token
-    #exec {
-    #  api_version = "client.authentication.k8s.io/v1beta1"
-    #  command     = "aws"
-    #  args        = ["eks", "get-token", "--cluster-name", "${var.name_prefix}-cluster"]
-    #}
+    #token                  = data.aws_eks_cluster_auth.default.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", "${var.name_prefix}-cluster"]
+    }
   }
 }
 
@@ -81,12 +81,12 @@ provider "kubernetes" {
   #apply_retry_count      = 5
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority.0.data) #  load_config_file       = false
-  token                  = data.aws_eks_cluster_auth.default.token
-  #exec {
-  #  api_version = "client.authentication.k8s.io/v1beta1"
-  #  command     = "aws"
-  #  args        = ["eks", "get-token", "--cluster-name", "${var.name_prefix}-cluster"]
-  #}
+  #token                  = data.aws_eks_cluster_auth.default.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", "${var.name_prefix}-cluster"]
+  }
 }
 
 /*
