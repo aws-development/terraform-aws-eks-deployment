1fcf8ff1b68ac848d7c75fc63d077528cf304dc0
Ravi Kumar <RjDev@users.noreply.github.com>
2026-04-20 12:36:58 +0530

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,9 @@
 resource "aws_ecr_repository" "ecr_repo" {
   name                 = "wiz-demo-repo"
   image_tag_mutability = "MUTABLE" # Or "IMMUTABLE" based on your needs
-  encryption_type = "KMS"
+    encryption_configuration {
+    encryption_type = "KMS"
+  }
   image_scanning_configuration {
     scan_on_push = true # Set to true if you want to scan images as they are pushed
   }
