b96d45f834f3b26f1519937140d9c305e0d4ee66
Ravi Kumar <RjDev@users.noreply.github.com>
2026-04-20 10:54:10 +0530

    Add AWS ECR repository configuration

---
+++ ecr.tf
@@ -0,0 +1,8 @@
+resource "aws_ecr_repository" "ecr_repo" {
+  name                 = "wiz-demo-repo"
+  image_tag_mutability = "MUTABLE" # Or "IMMUTABLE" based on your needs
+
+  image_scanning_configuration {
+    scan_on_push = true # Set to true if you want to scan images as they are pushed
+  }
+}
